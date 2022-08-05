f96248a2757102056a59f2ce93221d4cf5fa67b2
Johannes Aberidis <jo@e175.net>
2022-08-05 20:08:00 +0200

    use static create function

--- samples/PieChart.php
+++ samples/PieChart.php
@@ -11,6 +11,7 @@ use Sportlog\GoogleCharts\Charts\Base\ColumnType;
 use Sportlog\GoogleCharts\Charts\Base\DataTable;
 use Sportlog\GoogleCharts\Charts\Options\Common\ChartLegend\ChartLegend;
 use Sportlog\GoogleCharts\Charts\Options\Common\ChartLegend\ChartLegendPosition;
+use Sportlog\GoogleCharts\Charts\Options\PieChart\PieChartOptions;
 use Sportlog\GoogleCharts\Charts\Options\PieChart\PieChartSlice;
 use Sportlog\GoogleCharts\Charts\Options\PieChart\PieChartSliceCollection;
 use Sportlog\GoogleCharts\Charts\Options\PieChart\PieChartSliceText;
@@ -35,33 +36,16 @@ $chart->options->height = 500;
 $chart->options->pieHole = 0.4;
 
 // Slices
-$data = new DataTable();
-$data->addColumn(new Column(ColumnType::String, 'Language'));
-$data->addColumn(new Column(ColumnType::Number, 'Speakers (in millions)'));
-$data->addRows(
-    ['Assamese', 13],
-    ['Bengali', 83],
-    ['Bodo', 1.4],
-    ['Dogri', 2.3],
-    ['Gujarati', 46],
-    ['Hindi', 300],
-    ['Kannada', 38],
-    ['Kashmiri', 5.5],
-    ['Konkani', 5],
-    ['Maithili', 20],
-    ['Malayalam', 33],
-    ['Manipuri', 1.5],
-    ['Marathi', 72],
-    ['Nepali', 2.9],
-    ['Oriya', 33],
-    ['Punjabi', 29],
-    ['Sanskrit', 0.01],
-    ['Santhali', 6.5],
-    ['Sindhi', 2.5],
-    ['Tamil', 61],
-    ['Telugu', 74],
-    ['Urdu', 52]
-);
+$data = DataTable::fromArray([
+    ['Language', 'Speakers (in millions)'],
+    ['Assamese', 13], ['Bengali', 83], ['Bodo', 1.4],
+    ['Dogri', 2.3], ['Gujarati', 46], ['Hindi', 300],
+    ['Kannada', 38], ['Kashmiri', 5.5], ['Konkani', 5],
+    ['Maithili', 20], ['Malayalam', 33], ['Manipuri', 1.5],
+    ['Marathi', 72], ['Nepali', 2.9], ['Oriya', 33],
+    ['Punjabi', 29], ['Sanskrit', 0.01], ['Santhali', 6.5],
+    ['Sindhi', 2.5], ['Tamil', 61], ['Telugu', 74], ['Urdu', 52]
+]);
 
 $chart = $chartService->createPieChart('languageUse', $data);
 $chart->options->title = 'Indian Language Use';
@@ -76,22 +60,26 @@ $chart->options->slices->add(new PieChartSlice(offset: 0.4), 14);
 $chart->options->slices->add(new PieChartSlice(offset: 0.5), 15);
 
 // Pizza
-$data = new DataTable();
-$data->addColumn(new Column(ColumnType::String, 'Pizza'));
-$data->addColumn(new Column(ColumnType::Number, 'Populartiy'));
-$data->addRows(
-    ['Pepperoni', 33],
-    ['Hawaiian', 26],
-    ['Mushroom', 22],
-    ['Sausage', 10], // Below limit.
-    ['Anchovies', 9]
-);
+$options = new PieChartOptions();
+$options->title = 'Popularity of Types of Pizza';
+$options->height = 285;
+$options->width = 900;
+$options->sliceVisibilityThreshold = 0.2;
 
-$chart = $chartService->createPieChart('pizza', $data);
-$chart->options->title = 'Popularity of Types of Pizza';
-$chart->options->height = 285;
-$chart->options->width = 900;
-$chart->options->sliceVisibilityThreshold = 0.2;
+$chart = $chartService->createPieChart(
+    'pizza',
+    DataTable::fromArray(
+        [
+            ['Pizza', 'Populartiy'],
+            ['Pepperoni', 33],
+            ['Hawaiian', 26],
+            ['Mushroom', 22],
+            ['Sausage', 10], // Below limit.
+            ['Anchovies', 9] // Below limit.
+        ]
+    ),
+    $options
+);
 
 echo $chartService->render('dailyActivities');
 echo $chartService->render('languageUse');
